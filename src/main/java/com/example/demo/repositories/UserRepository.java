afae9d8775224a3ac8ab7f99a14aece258c69161
Ignacio Gonzalez <ignagonzalez2003@gmail.com>
2025-02-04 20:53:51 -0300

    feat: adding a JPAspcificationexecutor interface and new methods in repository

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -4,12 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.models.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
     Optional<User> findByUserIdAndDeleteAtIsNull(Long id);
 
@@ -17,6 +18,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmailAndDeleteAtIsNullAndIsBannedIsFalse(String email);
 
+    Optional<User> findByEmailAndDeleteAtIsNull(String email);
+
     Optional<User> findByEmail(String email);
+
+    List<User> findByEmailIn(List<String> usersEmailsToNotify);
 
 }
